ea235822271e2ae276a0c4ebb7f00eaff6bd9d72
eggh234 <eggh234@gmail.com>
2022-09-09 14:26:16 -0400

    resolving merge conflict

--- HW2.py
+++ HW2.py
@@ -1,2 +1,4 @@
 print(“Hello world! My favorite movie is Annabelle”)
 # (Replace “The Room” with your actual favorite movie.)
+print("My favorite song is ABC by School") 
+# swap in your actual favorite song
